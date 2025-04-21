db60cfc428b8676cca2d0e9bd531f64d0e026e2b
sudeepgouda26 <sudeepgouda26@gmail.com>
2025-04-21 22:56:21 +0530

    first page animation

--- Project2/index.js
+++ Project2/index.js
@@ -12,3 +12,51 @@ tl.from("#part2 button",{
     opacity:0,
 
 })
+
+tl.from("#leftpart #heading>h1",{
+    duration:0.9,
+    opacity:0,
+    x :-100,
+    stagger:0.2,
+    ease:"power2.out"
+})
+tl.from("#leftpart :nth-child(2) ",{
+    duration:0.9,
+    opacity:0,
+    x :100,
+    stagger:0.2,
+    ease:"power2.out"
+})
+tl.from("#leftpart :nth-child(3) ",{
+    duration:0.9,
+    opacity:0,
+    y:100,
+    stagger:0.2,
+    ease:"power2.out"
+},"-=0.5")
+
+tl.from("#rightpart img",{
+    duration:1,
+    opacity:0,
+    x:100,
+    stagger:0.2,
+    ease:"power2.out"
+},"-=1.05")
+
+tl.from("#companies img ",{
+  
+    scrollTrigger:{
+        trigger:"#companies",
+        scroller:"body",
+        markers:true,
+        start:"top 80%",
+        end:"top 70%",
+        scrub:1,
+    },
+    opacity:0,
+    x:-100,
+
+    stagger:0.3,
+})
+   
+ 

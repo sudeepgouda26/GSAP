a54523e8a47b8b7ec0f606a1260d7435355e5089
sudeepgouda26 <sudeepgouda26@gmail.com>
2025-04-25 22:28:24 +0530

    loader

--- OBYS/script.js
+++ OBYS/script.js
@@ -8,12 +8,29 @@ gsap.from("#loader h2",{
 let timer = document.querySelector("#line-part1 h5");
 let grow =0;
 
-setInterval(()=>{
-if(grow<100){
-    grow++;
-    timer.innerHTML= grow ;
-}else{
-    grow=100;
-    timer.innerHTML= grow ;
-}
-},35)
+
+
+let tl = gsap.timeline();
+tl.from("#loader #line-part1,#now",{
+    opacity:0,
+    duration:1,
+    delay:1,
+ 
+   
+    onStart:function () {
+        setInterval(()=>{
+            if(grow<100){
+                grow++;
+                timer.innerHTML= grow ;
+            }else{
+                grow=100;
+                timer.innerHTML= grow ;
+            }
+            },33)
+        
+    }
+})
+tl.to("#loader",{
+    opacity:0,
+    delay:4
+})

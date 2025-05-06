9b98a66e2eaf3058862fb66eaae4c8de7e7c60d7
sudeepgouda26 <sudeepgouda26@gmail.com>
2025-05-06 23:25:59 +0530

    locomotive js

--- OBYS/script.js
+++ OBYS/script.js
@@ -1,4 +1,50 @@
 
+function locomotivejs(){
+    gsap.registerPlugin(ScrollTrigger);
+
+
+// --- SETUP START ---
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, {duration: 0, disableLerp: true}) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+ScrollTrigger.defaults({ scroller: "#main" });
+// --- SETUP END ---
+
+
+
+
+
+
+
+
+
+
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
+locomotivejs();
+
 function loader(){
     gsap.from("#loader h2",{
         y:160,
@@ -58,6 +104,10 @@ function loader(){
     tl.from("#page1 h1,h2",{
         y:130,
         stagger:0.25,
+    })
+    tl.from("#page2 ",{
+    
+        opacity:0,
     })
 }
 loader();
